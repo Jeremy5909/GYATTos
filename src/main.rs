708ca700c5621cc5a2e4cf14420b17e5cd13a81f
Jeremy5909 <jeremyherczeg@gmail.com>
2024-05-13 10:01:59 -0700

    tests

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
 
 use core::panic::PanicInfo;
 
@@ -15,5 +26,16 @@ fn panic(info: &PanicInfo) -> ! {
 // Use C calling convention
 pub extern "C" fn _start() -> ! {
     println!("Hello, World!");
-    panic!("Uh oh, nothing went wrong!");
+
+    #[cfg(test)]
+    test_main();
+    
+    loop{}
+}
+
+#[test_case]
+fn trivial_assertioin() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
